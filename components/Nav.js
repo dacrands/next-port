52852a1691d229f5007b3c7c34a153874136633c
da crands <davecrands@gmail.com>
2018-10-21 21:12:45 -0400

    NExt head config

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,13 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const Nav = () => (
-  <nav className="navbar">
+  <div>
+    <Head>
+      <title>David Crandall</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <nav className="navbar">
     <ul className="navbar__list">
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
@@ -9,6 +15,8 @@ const Nav = () => (
       <Link href="/contact"><a>Contact</a></Link>                
     </ul>
   </nav>
+  </div>
+  
 )
 
 export default Nav;
