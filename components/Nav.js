0bee0d6fd79e1d7170d1fd9543100f6f1b104bda
da crands <davecrands@gmail.com>
2018-10-21 22:27:54 -0400

    meta device-width

--- components/Nav.js
+++ components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => (
   <div>
     <Head>
       <title>David Crandall</title>
-      <meta name="viewport" content="width=device-width" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
     <nav className="navbar">
     <ul className="navbar__list">
