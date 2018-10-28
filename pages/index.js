8405f2961a91ef03ccd899bcc676017fe047e099
da crands <davecrands@gmail.com>
2018-10-28 12:51:18 -0400

    Landing component, scroll indicator

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Nav from '../components/Nav';
+import Landing from '../components/Landing';
 import Projects from '../components/Projects';
 import About from '../components/About';
 import Contact from '../components/Contact';
@@ -13,35 +14,12 @@ export default () =>
       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </Head>
     <Nav />
-    <div className="landing__container">
-    <div className="landing__skew"></div>
-    <div className="landing">
-      <div>
-        <h1 className="primary">David Crandall</h1>
-        <h2 className="font--light ">Full-stack Web Developer</h2>
-        <p>I build websites that make sense. From email-authentication to
-          responsive web-design, I love all aspects of building a website.
-        </p>
-        <p>As a front-end developer, I focus on accessibility, performance, and beauty.
-          As a back-end developer, I am all about security.</p>
-        <br/>
-        <div>
-          <a href="#projects">
-            Portfolio &rsaquo;
-          </a>
-          <a href="https://dacrands.github.io">
-            Blog &rsaquo;
-          </a>
-        </div>
-      </div>
-    </div>    
-    </div>
+    <Landing />
     <div className="container">
       <Projects />
-    </div>
-    {/* <div className="container"> */}
-      <About />
-    {/* </div> */}
+    </div>    
+    <About /> 
+       
     <div>
     <Contact />
     </div>
