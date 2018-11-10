a4f9515104115fc6c0457f6426d8c4d91f83977b
da crands <davecrands@gmail.com>
2018-11-09 19:43:17 -0500

    Footer redesign, add resume

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,13 @@
 const Footer = () => (
   <footer className="footer">
-    <div className="container">
+    <div className="">
       <ul>
         <li><a href="">Github</a></li>
-        <li><a href="">Blog</a></li>
-        <li><a href="" /></li>
+        <li><a href="https://dacrands.github.io">Blog</a></li>        
+        <li><a href="../static/resume.pdf" target="_blank" rel="noopener">Resume</a></li>
+        <li><a href="mailto:davecrands@gmail.com">Email</a></li>
       </ul>
-      <p>Built by David Crandall</p>
+      <p> © 2018 by David Crandall. All rights reserved. 🌏</p>
     </div>
   </footer>
 );
